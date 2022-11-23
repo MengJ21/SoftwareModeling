d7ce33a0b47dc4b43b87682f8bf47db910335ad9
MengJ <1747153981@qq.com>
2022-11-23 17:41:24 +0800

    新建软件建模项目

---
+++ src/main/java/com/modeling/software/SoftwareModelingApplication.java
@@ -0,0 +1,13 @@
+package com.modeling.software;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareModelingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareModelingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/modeling/software/SoftwareModelingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modeling.software;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareModelingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
